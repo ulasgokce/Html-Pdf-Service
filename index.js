fb7b6bf13e920eb0a1df82279028061711f36f20
ulasgokce <ulasgokce98@gmail.com>
2022-11-02 18:41:15 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+const fs = require('fs');
+var bodyParser = require("body-parser");
+const pdf = require('html-pdf');
+
+var jsonParser = bodyParser.json();
+
+const port = process.env.PORT || 5002;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log("getStarted", new Date());
+});
+app.post("/get-base64", jsonParser,function (req, res) {
+
+    var html = req.body.html;
+    var options = { 
+    "format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+    "orientation": "portrait", // portrait or landscape
+     };
+    
+    pdf.create(html, options).toBuffer((err, buffer) => {
+       res.send(buffer.toString('base64'))
+      })
+});
